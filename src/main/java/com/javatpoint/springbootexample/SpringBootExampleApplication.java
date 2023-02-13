124d742d4920d07a40dbb625bb97c462922f20a2
Asritha <asritham0699@gmail.com>
2023-02-13 13:08:40 +0530

    first commit using sts

--- src/main/java/com/javatpoint/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/javatpoint/springbootexample/SpringBootExampleApplication.java
@@ -1,11 +1,12 @@
 package com.javatpoint.springbootexample;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-public class SpringBootExampleApplication 
-{
-public static void main(String[] args) 
-{
-SpringApplication.run(SpringBootExampleApplication.class, args);
-}
+public class SpringBootExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+		System.out.println("First commit");
+	}
 }
